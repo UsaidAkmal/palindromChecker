b7db9cd099e38fcf9c0e013a841b890a5f34ac4e
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-07-19 23:34:27 +0700

    done

--- palcek.py
+++ palcek.py
@@ -4,3 +4,14 @@ print(f'''
 ▙▌█▌▐▖▙▖▙▖▛▖
 ▌ palindrom checker with python language.           
 ''')
+
+def palchecker(word):
+    word = kata.lower()
+    return word == word[::-1] 
+
+userWordInput = input("Masukan kata: ")
+
+if palchecker(userWordInput):
+    print(f"{userWordInput} adalah palindrom")
+else:
+    print(f"{userWordInput} bukan palindrom")
